c1c3511b64da2b60a3840c6b75c4ef8790a4618f
falchon100 <ovnicrofordz@gmail.com>
2023-09-17 16:54:43 -0300

    entrega tarea 2

--- Tarea2/tp2_backend.php
+++ Tarea2/tp2_backend.php
@@ -8,38 +8,55 @@
 <body>
 <?php
 
+
+/* Crear una variable n y validar que sea un número positivo. */
 $n = 2;
-
-
 
 print("el numero elegido fue $n <br><br>" );
 
 if ($n>0 ){
-    print('1.es un numero positivo <br><br>');
+    print("$n es un numero positivo <br><br>");
 }else{
-    print('PASO EQUIVOCADO <br><br>');
+    print("$n no un numero positivo <br><br>");
 }
 
-if ($n>1 && $n<10){
-    print('2.es un numero mayor a 1 y menor a 10 <br><br>');
-}else{
-    print('PASO EQUIVOCADO <br><br>');
-}
+/* Crear una variable n y validar que sea un número mayor a 1 y menor a 10 */
+$n2 = 3 ;
+print("_______________________________<br><br>" );
 
+print("el numero elegido fue $n2 <br><br>" );
 
-if ($n>10 || $n<2){
-    print('es un numero mayor a 10 o menor a 2');
+if ($n2>1 && $n2<10){
+    print("$n2 es un numero mayor a 1 y menor a 10 <br><br>");
 }else{
-    print('es cualquier otro numero <br>');
+    print("$n2 no es un numero mayor a 1 y menor a 10 <br><br>");
 }
+print("_______________________________<br><br>" );
+
+/* Crear una variable n y validar que sea un número mayor a 10 o menor a 2. */
+$n3 = 4;
+print("el numero elegido fue $n3 <br><br>" );
+
+if ($n3>10 || $n3<2){
+    print(" $n3 es un numero mayor a 10 o menor a 2 <br>");
+}else{
+    print("$n3 no es un numero mayor a 10 ni menor a 2<br>");
+}
+print("_______________________________<br><br>" );
+
+/* Crear dos variables, una con nombre numero1 y otra con el nombre de numero2. Si numero1 es
+mayor a numero2, mostrar por pantalla, la suma y la resta. Si numero2 es mayor a numero1,
+mostrar por pantalla la multiplicación, la división entera y el resto de la división. Si numero1 y
+numero2 son iguales, mostrar el siguiente mensaje “Los números ingresados son iguales” */
 
 $numero1= 4;
 $numero2= 2;
-
+print("el primer numero elegido fue $numero1 <br><br>" );
+print("el segundo numero elegido fue $numero2 <br><br>" );
 if($numero1>$numero2){
     $suma = $numero1+$numero2;
     $resta = $numero1-$numero2;
-    print("la suma es $suma y la esta es $resta ");
+    print("la suma es $suma y la resta es $resta ");
 
 }elseif($numero1 == $numero2){
     print("los numeros son iguales");
@@ -59,10 +76,3 @@ else{
 
 
 
-<!-- 1. Crear una variable n y validar que sea un número positivo.
-2. Crear una variable n y validar que sea un número mayor a 1 y menor a 10.
-3. Crear una variable n y validar que sea un número mayor a 10 o menor a 2.
-4. Crear dos variables, una con nombre numero1 y otra con el nombre de numero2. Si numero1 es
-mayor a numero2, mostrar por pantalla, la suma y la resta. Si numero2 es mayor a numero1,
-mostrar por pantalla la multiplicación, la división entera y el resto de la división. Si numero1 y
-numero2 son iguales, mostrar el siguiente mensaje “Los números ingresados son iguales”. -->
