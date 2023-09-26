635c51a875cb875aadcf1263c25e124d3d4a9cba
Angel-trabajo <ovnicrofordz@gmail.com>
2023-09-26 15:48:38 -0300

    modificado

--- Tarea4/index.php
+++ Tarea4/index.php
@@ -1,7 +1,20 @@
 <?php
+//Guardo el usuario y contraseña enviado en el form
 $user = $_POST ["user"];
 $pass = $_POST ["pass"];
 
-echo $user,$pass;
+//Variables correctas para login
+
+$userCorrecto = "prueba@gmail.com";
+$passCorrecta = "asd123";
+
+
+if($user == $userCorrecto && $pass == $passCorrecta){
+    header("Location: home.html");
+    exit;
+}else{
+    header("location:https://www.figma.com/error");
+    exit;
+}
 
 ?>
